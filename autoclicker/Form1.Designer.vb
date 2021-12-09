d76d04dda16d5cb9dab28c58a9ad0757106b8c0c
Aveneid <dj.exers@gmail.com>
2021-12-09 23:21:23 +0100

    Initial commit

---
+++ autoclicker/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Record"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 42)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(237, 199)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(134, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(116, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Execute"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 248)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 52)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Insert - set cursor pos" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "PageUp - left click" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "PageDown - right click" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Home - dela" &
+    "y" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(174, 277)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 308)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(262, 22)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.Stretch = False
+        Me.StatusStrip1.TabIndex = 5
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(60, 17)
+        Me.ToolStripStatusLabel1.Text = "Waiting...."
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(188, 257)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(45, 14)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "X"
+        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(262, 330)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Autoclicker"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents Button4 As Button
+End Class
---
+++ autoclicker/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Dim recording As Boolean = False
+    Dim t As New Thread(AddressOf execMacro)
+    Public Structure POINTAPI
+        Public Property X As Int32
+        Public Property Y As Int32
+    End Structure
+
+    Dim p As POINTAPI
+    Public Declare Function GetCursorPos Lib "user32" (ByRef lpPoint As POINTAPI) As Boolean
+    Public Declare Auto Function SetCursorPos Lib "User32.dll" (ByVal X As Integer, ByVal Y As Integer) As Long
+    Private Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If recording = False Then
+            Button1.Text = "Stop"
+            recording = True
+            Me.KeyPreview = True
+        Else
+            Button1.Text = "Record"
+            recording = False
+            Me.KeyPreview = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If recording = False Then
+            t = New Thread(AddressOf execMacro)
+            Select Case t.ThreadState
+                Case ThreadState.Suspended
+                    t.Resume()
+                    ToolStripStatusLabel1.Text = "Running"
+                Case ThreadState.Stopped
+                Case ThreadState.Aborted
+                Case ThreadState.Unstarted
+                    t.Start()
+                    ToolStripStatusLabel1.Text = "Running"
+                Case ThreadState.Running
+                    t.Abort()
+                    ToolStripStatusLabel1.Text = "Waiting"
+            End Select
+        End If
+    End Sub
+
+    Sub execMacro()
+        SetCursorPos(0, 0)
+        For Each i As ListViewItem In ListBox1.Items
+            Select Case i.Text.Substring(1, 1)
+                Case "#"
+                    SetCursorPos(i.Text.Substring(2).Split(",")(0), i.Text.Substring(3).Split(",")(1))
+                Case "("
+                    mouse_event(&H2, 0, 0, 0, 0)
+                    mouse_event(&H4, 0, 0, 0, 0)
+                Case ")"
+                    mouse_event(&H8, 0, 0, 0, 0)
+                    mouse_event(&H10, 0, 0, 0, 0)
+                Case "!"
+                    t.Sleep(Int(i.Text.Substring(2)))
+            End Select
+            If i.Index = ListBox1.Items.Count Then
+                t.Abort()
+                StatusStrip1.Text = "Waiting...."
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Escape
+                Me.KeyPreview = False
+                recording = False
+                Button1.Text = "Record"
+            Case Keys.Insert
+                GetCursorPos(p)
+                ListBox1.Items.Add(New ListViewItem(" # " & CStr(p.X) & "," & CStr(p.Y)))
+            Case Keys.PageUp
+                ListBox1.Items.Add(New ListViewItem(" ( Left click"))
+            Case Keys.PageDown
+                ListBox1.Items.Add(New ListViewItem(" ( Right Click"))
+            Case Keys.Home
+                Dim ms = InputBox("Time in ms: ")
+                While Not IsNumeric(ms) Or ms < 0
+                    ms = InputBox("Time in ms: ")
+                End While
+                If ms.Length > 0 Then
+                    ListBox1.Items.Add(New ListViewItem(" ! " & ms))
+                End If
+            Case Keys.Delete
+                ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+        End Select
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Add(New ListViewItem(" # 666,300"))
+        ListBox1.Items.Add(New ListViewItem(" ! 5000"))
+        ListBox1.Items.Add(New ListViewItem(" ( "))
+        ListBox1.Items.Add(New ListViewItem(" ) "))
+        ListBox1.Items.Add(New ListViewItem(" ! 5000"))
+        ListBox1.Items.Add(New ListViewItem(" # 100,100"))
+        ListBox1.Items.Add(New ListViewItem(" ! 5000"))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Clear()
+    End Sub
+End Class
